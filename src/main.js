98c830894bc0c7209f2dc8f8c0ea97f581efc219
aj-floater <80073564+aj-floater@users.noreply.github.com>
2023-05-09 19:58:10 +0100

    Update main.js
    added an alert

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import {Viewport} from "../modules/viewport.js"
 import {BIEM} from "../modules/biem.js"
 import {Box} from "../modules/box.js"
 
-alert("this is a test")
+alert("This is a basic online robot arm simulator. \n\
+This version has limited server side functionality (due to security reasons)\n\
+So the only functional sub-program is the BIEM (Basic Integrated Environment Modeller\n\
+I may add a hard coded robot and the ability to copy and paste code into the code editor for this version...\n\
+but in the meantime checkout https://github.com/aj-floater/robot-arm-simulator-2 to host your own version\
+of this program on your own local server!")
 
 let viewport = new Viewport("viewport", null); // red 0xff0808
 let programmer = new Programmer("programmer");
