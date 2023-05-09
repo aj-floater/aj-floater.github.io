1082a0889c63bd0737e07ee88455692ae9faa8d0
aj-floater <80073564+aj-floater@users.noreply.github.com>
2023-05-09 19:59:31 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import {Box} from "../modules/box.js"
 alert("This is a basic online robot arm simulator. \n\
 This version has limited server side functionality (due to security reasons)\n\
 So the only functional sub-program is the BIEM (Basic Integrated Environment Modeller\n\
-I may add a hard coded robot and the ability to copy and paste code into the code editor in the future...\n\
+I may add a hard coded robot and the ability to copy and paste code into the code editor for this version in the future...\n\
 but in the meantime checkout https://github.com/aj-floater/robot-arm-simulator-2 to host your own version\
 of this program on your own local server for full functionality!")
 
