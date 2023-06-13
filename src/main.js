9f76770623ee62d33e116edfe874687f9b72f8f4
Archie James <arjames1618@gmail.com>
2023-06-13 21:37:59 +0100

    Improved welcome message

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import {Viewport} from "../modules/viewport.js"
 import {BIEM} from "../modules/biem.js"
 import {Box} from "../modules/box.js"
 
-alert("This is a basic online robot arm simulator. \n\
-This version has limited server side functionality (due to security reasons)\n\
-I have added a hard coded robot and the ability to read some static files from the programmer\n\n\
-Checkout https://github.com/aj-floater/robot-arm-simulator-2 to host your own version\
-of this program on your own local server!")
+alert("This is a basic online robot arm simulator \
+that has limited server side functionality (because this is a static website)\n\n\
+Which means that importing programs and robots is sadly not possible...\n\n\
+So, to make up for the lost functionality, I have added a hard coded robot and the ability to read some static files into the programmer to make up for the lost functionality!\n\n\
+Checkout https://github.com/aj-floater/robot-arm-simulator-2 to host \
+this program on your own for the complete feature set :)")
 
 let viewport = new Viewport("viewport", "uploads/robot.urdf"); // red 0xff0808
 let programmer = new Programmer("programmer");
